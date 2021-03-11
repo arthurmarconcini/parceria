3ccc2a3d09f92c959e77c9db3785a1aa399272c1
Arthur Marconcini <arthur.m.ramos@hotmail.com>
2021-03-11 20:47:29 -0300

    carousel autoplay and smooth adjusts

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -1,14 +1,35 @@
-let slider = document.querySelector('.slides');
-
-new Glider(slider, {
+let slider = new Glider(document.querySelector('.slides'), {
   slidesToShow: 1,
   slidesToScroll: 1,
   draggable: true,
-  dots: '.dots',
-  arrows: {
-    prev: '.glider-prev',
-    next: '.glider-next'
-  },
+  dots: '.dots',  
   scrollLock: true,
-  rewind: true,  
+  rewind: true, 
 });
+
+function sliderAuto(slider, miliseconds) {
+ slider.isLastSlide = function() {
+   return slider.page >= slider.dots.childElementCount - 1;
+ }
+
+ var slide = function() {
+   slider.slideTimeout = setTimeout(function() {
+     function slideTo() {
+       return slider.isLastSlide() ? 0 : slider.page + 1;
+     }
+
+     slider.scrollItem(slideTo(), true);
+   }, miliseconds);
+ }
+
+ slider.ele.addEventListener('glider-animated', function(event) {
+   window.clearInterval(slider.slideTimeout);
+   slide();
+ });
+
+ slide();
+}
+
+sliderAuto(slider, 5000);
+
+
